888da0c98d046e28006b92c8ca5a49a4e18ce7c0
Corentin Bacqué-Cazenave <corentin.bacque-cazenave@etu.u-bordeaux.fr>
2023-02-20 15:36:52 +0100

    Revert "Name test function "sould_"
    This reverts commit e248d281a1175c12b71f4772f14717ced5cfa6b5.

--- php/tests/MoneyProblem/BankTest.php
+++ php/tests/MoneyProblem/BankTest.php
@@ -10,17 +10,17 @@ use PHPUnit\Framework\TestCase;
 class BankTest extends TestCase
 {
 
-    public function should_convert_eur_to_usd_returns_float()
+    public function test_convert_eur_to_usd_returns_float()
     {
         $this->assertEquals(12, Bank::create(Currency::EUR(), Currency::USD(), 1.2)->convert(10, Currency::EUR(), Currency::USD()));
     }
 
-    public function should_convert_eur_to_eur_returns_same_value()
+    public function test_convert_eur_to_eur_returns_same_value()
     {
         $this->assertEquals(10, Bank::create(Currency::EUR(), Currency::USD(), 1.2)->convert(10, Currency::EUR(), Currency::EUR()));
     }
 
-    public function should_convert_throws_exception_on_missing_exchange_rate()
+    public function test_convert_throws_exception_on_missing_exchange_rate()
     {
         $this->expectException(MissingExchangeRateException::class);
         $this->expectExceptionMessage('EUR->KRW');
@@ -28,7 +28,7 @@ class BankTest extends TestCase
         Bank::create(Currency::EUR(), Currency::USD(), 1.2)->convert(10, Currency::EUR(), Currency::KRW());
     }
 
-    public function should_convert_with_different_exchange_rates_returns_different_floats()
+    public function test_convert_with_different_exchange_rates_returns_different_floats()
     {
         $bank = Bank::create(Currency::EUR(), Currency::USD(), 1.2);
 
