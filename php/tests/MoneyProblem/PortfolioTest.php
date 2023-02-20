89a34b6aea524b36c6fa28abacbd3daf77a09f6d
Bryan <bryanparra47310@gmail.com>
2023-02-20 17:48:15 +0100

    PortolioTest

---
+++ php/tests/MoneyProblem/PortfolioTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests\MoneyProblem\Domain;
+
+use MoneyProblem\Domain\Currency;
+use MoneyProblem\Domain\MoneyCalculator;
+use PHPUnit\Framework\TestCase;
+
+class PortfolioTest extends TestCase
+{
+}
