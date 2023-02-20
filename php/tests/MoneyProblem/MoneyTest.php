e248d281a1175c12b71f4772f14717ced5cfa6b5
Corentin Bacqué-Cazenave <corentin.bacque-cazenave@etu.u-bordeaux.fr>
2023-02-20 15:31:15 +0100

    Name test function "sould_ "

--- php/tests/MoneyProblem/MoneyTest.php
+++ php/tests/MoneyProblem/MoneyTest.php
@@ -8,18 +8,18 @@ use PHPUnit\Framework\TestCase;
 
 class MoneyTest extends TestCase
 {
-    public function test_add_in_usd_returns_value()
+    public function should_add_in_usd_returns_value()
     {
         $this->assertIsFloat(MoneyCalculator::add(5, Currency::USD(), 10));
         $this->assertNotNull(MoneyCalculator::add(5, Currency::USD(), 10));
     }
 
-    public function test_multiply_in_euros_returns_positive_number()
+    public function should_multiply_in_euros_returns_positive_number()
     {
         $this->assertLessThan(MoneyCalculator::times(10, Currency::USD(), 2), 0);
     }
 
-    public function test_divide_in_korean_won_returns_float()
+    public function should_divide_in_korean_won_returns_float()
     {
         $this->assertEquals(MoneyCalculator::divide(4002, Currency::USD(), 4), 1000.5);
     }
