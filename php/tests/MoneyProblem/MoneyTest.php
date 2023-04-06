657acb956525926fe6a8fffcef0b651f21ebd8e4
Bryan <bryanparra47310@gmail.com>
2023-04-06 17:50:18 +0200

    d

--- php/tests/MoneyProblem/MoneyTest.php
+++ php/tests/MoneyProblem/MoneyTest.php
@@ -2,12 +2,22 @@
 
 namespace Tests\MoneyProblem\Domain;
 
+use MoneyProblem\Domain\Money;
 use MoneyProblem\Domain\Currency;
 use MoneyProblem\Domain\MoneyCalculator;
 use PHPUnit\Framework\TestCase;
 
 class MoneyTest extends TestCase
 {
+    public function test_create_money_object()
+    {
+        $money = new Money(10, Currency::USD());
+
+        $this->assertInstanceOf(Money::class, $money);
+        $this->assertEquals(10, $money->getAmount());
+        $this->assertEquals(Currency::USD(), $money->getCurrency());
+    }
+
     public function test_should_add_in_usd()
     {
         $this->assertIsFloat(MoneyCalculator::add(5, Currency::USD(), 10));
